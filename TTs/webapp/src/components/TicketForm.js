e0cb77e3321c48b523658406ce8b86ac8842d035
Amadeu Pereira <up201605646@fe.up.pt>
2020-05-17 22:25:10 +0100

    ticket form component

--- TTs/webapp/src/components/TicketForm.js
+++ TTs/webapp/src/components/TicketForm.js
@@ -4,8 +4,12 @@ import {
     Container,
     Grid,
     TextField,
-    Button
+    Button,
+    Snackbar,
 } from "@material-ui/core";
+import {
+    Alert
+} from "@material-ui/lab";
 import { sendTicket } from "../ApiServices.js";
 
 
@@ -29,79 +33,106 @@ const TicketForm = () => {
         description: ""
     });
 
+    const [snackbarOpen, setSnackbarOpen] = React.useState({
+        open: false,
+        message: "",
+        severity: ""
+    });
+
     const handleChange = (event) => {
         setState({ ...state, [event.target.name]: event.target.value });
     }
 
-    const handleSubmit = (event) => {
+    const snackbarClose = (event) => {
+        setSnackbarOpen({ open: false });
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         console.log(state);
-        sendTicket(state);
+        const res = await sendTicket(state);
+        if (res.status === 201) {
+            setSnackbarOpen({ open: true, message: "Ticket submited!", severity: "success" });
+        } else {
+            setSnackbarOpen({ open: true, message: "Ticket submission failed!", severity: "error" })
+        }
     }
 
     return (
-        <Container component="main">
-            <form onSubmit={handleSubmit} className={classes.form}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            name="name"
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="name"
-                            label="Name"
-                            autoFocus
-                            onChange={handleChange}
-                        />
+        <>
+            <Container component="main">
+                <form onSubmit={handleSubmit} className={classes.form}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                name="name"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="name"
+                                label="Name"
+                                autoFocus
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email"
+                                name="email"
+                                type="email"
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="title"
+                                label="Title"
+                                name="title"
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                multiline
+                                rows={3}
+                                name="description"
+                                label="Description"
+                                id="description"
+                                onChange={handleChange}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email"
-                            name="email"
-                            type="email"
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="title"
-                            label="Title"
-                            name="title"
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            multiline
-                            rows={3}
-                            name="description"
-                            label="Description"
-                            id="description"
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                </Grid>
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                >
-                    Submit
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Submit
                 </Button>
-            </form>
-        </Container>
+                </form>
+            </Container>
+            <Snackbar
+                open={snackbarOpen.open}
+                autoHideDuration={6000}
+                onClose={snackbarClose}
+            >
+                <Alert onClose={snackbarClose} severity={snackbarClose.severity}>
+                    {snackbarOpen.message}
+                </Alert>
+            </Snackbar>
+        </>
+
     );
 }
 
