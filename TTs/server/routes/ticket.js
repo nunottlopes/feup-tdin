c71ea2cb5e68357d3f3984ea81f5ff95f4735124
Amadeu Pereira <up201605646@fe.up.pt>
2020-05-21 13:25:20 +0100

    send mail

--- TTs/server/routes/ticket.js
+++ TTs/server/routes/ticket.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const Ticket = require("../models/ticket");
 const Secondary = require("../models/secondary");
+const nodemailer = require("nodemailer");
 
 router.get("/:id", (req, res) => {
     Ticket.findById(req.params.id, (err, result) => {
@@ -60,16 +61,49 @@ router.put("/:id/solve", (req, res) => {
         response: req.body.response
     };
 
-    Ticket.findByIdAndUpdate(req.params.id, update, (err, result) => {
+    Ticket.findByIdAndUpdate(req.params.id, update, { new: true }, (err, result) => {
         if (err) {
             res.status(500).send(err);
         } else {
             res.status(200).send(result);
+            sendEmail(result);
+        }
+    });
+});
+
+const sendEmail = (ticket) => {
+    console.log("Sending email to " + ticket.email);
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "tdintts@gmail.com",
+            pass: "tdin2020"
         }
     });
 
-    //TODO: Send email
-});
+    const mailOptions = {
+        from: "tdin-tts@gmail.com",
+        to: ticket.email,
+        subject: "The ticket you submitted has been solved!",
+        html: `<h1>The ticket you submitted has been solved!</h1> \
+        <br></br> \
+        <h2>Your ticket:</h2> \
+        <h4>Title:</h4>${ticket.title} \
+        <h4>Description:</h4>${ticket.description} \
+        <h4>Submitted at:</h4>${ticket.createdAt} \
+        <h2>Was solved by ${ticket.solver}</h2> \
+        <h4>Response:</h4>${ticket.response}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
+
+}
 
 router.put("/:id/assign", (req, res) => {
 
